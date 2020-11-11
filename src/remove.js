f69ab192d068935a481e0dab730cf1b3ff675775
Jeow Li Huan <huan086@gmail.com>
2020-11-12 01:40:04 +0800

    Fix `remove` mutator removing more fields than expected. Revert changes from #39 that moves `fieldSubscribers`, causing the wrong field to be notified.

--- src/remove.js
+++ src/remove.js
@@ -1,12 +1,12 @@
 // @flow
 import type { MutableState, Mutator, Tools } from 'final-form'
-import moveFieldState from './moveFieldState'
+import copyField from './copyField'
 import { escapeRegexTokens } from './utils'
 
 const remove: Mutator<any> = (
   [name, index]: any[],
   state: MutableState<any>,
-  { changeValue, renameField, getIn, setIn }: Tools<any>
+  { changeValue, getIn, setIn }: Tools<any>
 ) => {
   let returnValue
   changeValue(state, name, (array: ?(any[])): any[] => {
@@ -19,14 +19,12 @@ const remove: Mutator<any> = (
   // now we have to remove any subfields for our index,
   // and decrement all higher indexes.
   const pattern = new RegExp(`^${escapeRegexTokens(name)}\\[(\\d+)\\](.*)`)
-  const backup = { ...state, fields: { ...state.fields } }
+  const newFields = {}
   Object.keys(state.fields).forEach(key => {
     const tokens = pattern.exec(key)
     if (tokens) {
       const fieldIndex = Number(tokens[1])
       if (fieldIndex === index) {
-        // delete any subfields for this array item
-        delete state.fields[key]
         // delete any submitErrors for this array item
         // if the root key of the delete index
         if (key === `${name}[${index}]`) {
@@ -38,19 +36,24 @@ const remove: Mutator<any> = (
             state = setIn(state, path, submitErrors)
           }
         }
-      } else if (fieldIndex > index) {
-        // shift all higher ones down
-        delete state.fields[key]
+
+        return
+      }
+
+      if (fieldIndex > index) {
+        // Shift all higher indices down
         const decrementedKey = `${name}[${fieldIndex - 1}]${tokens[2]}`
-        if (backup.fields[decrementedKey]) {
-          moveFieldState(state, backup.fields[key], decrementedKey, backup)
-        } else {
-          // take care of setting the correct change, blur, focus, validators on new field
-          renameField(state, key, decrementedKey)
-        }
+        copyField(state.fields, key, newFields, decrementedKey)
+        return
       }
     }
+
+    // Keep this field that does not match the name,
+    // or has index smaller than what is being removed
+    newFields[key] = state.fields[key]
   })
+
+  state.fields = newFields
   return returnValue
 }
 
