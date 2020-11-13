71bebc75934e4c9bb161651bd88ad7b5029044d5
Jeow Li Huan <huan086@gmail.com>
2020-11-13 15:26:55 +0800

    Optimize `move`, removing the need to move field state to a temporary field or looping over fields many times.

--- src/move.js
+++ src/move.js
@@ -1,9 +1,7 @@
 // @flow
 import type { MutableState, Mutator, Tools } from 'final-form'
-import moveFields from './moveFields'
-import restoreFunctions from './restoreFunctions'
-
-const TMP: string = 'tmp'
+import copyField from './copyField'
+import { escapeRegexTokens } from './utils'
 
 const move: Mutator<any> = (
   [name, from, to]: any[],
@@ -21,34 +19,44 @@ const move: Mutator<any> = (
     return copy
   })
 
-  //make a copy of a state for further functions restore
-  const backupState = { ...state, fields: { ...state.fields } }
-
-  // move this row to tmp index
-  const fromPrefix = `${name}[${from}]`
-  moveFields(name, fromPrefix, TMP, state)
-
-  if (from < to) {
-    // moving to a higher index
-    // decrement all indices between from and to
-    for (let i = from + 1; i <= to; i++) {
-      const innerFromPrefix = `${name}[${i}]`
-      moveFields(name, innerFromPrefix, `${i - 1}`, state)
-    }
+  const newFields = {}
+  const pattern = new RegExp(`^${escapeRegexTokens(name)}\\[(\\d+)\\](.*)`)
+  let lowest
+  let highest
+  let increment
+  if (from > to) {
+    lowest = to
+    highest = from
+    increment = 1
   } else {
-    // moving to a lower index
-    // increment all indices between to and from
-    for (let i = from - 1; i >= to; i--) {
-      const innerFromPrefix = `${name}[${i}]`
-      moveFields(name, innerFromPrefix, `${i + 1}`, state)
-    }
+    lowest = from
+    highest = to
+    increment = -1
   }
+  Object.keys(state.fields).forEach(key => {
+    const tokens = pattern.exec(key)
+    if (tokens) {
+      const fieldIndex = Number(tokens[1])
+      if (fieldIndex === from) {
+        const newKey = `${name}[${to}]${tokens[2]}`
+        copyField(state.fields, key, newFields, newKey)
+        return
+      }
+      
+      if (lowest <= fieldIndex && fieldIndex <= highest) {
+        // Shift all indices
+        const newKey = `${name}[${fieldIndex + increment}]${tokens[2]}`
+        copyField(state.fields, key, newFields, newKey)
+        return
+      }
+    }
 
-  // move from tmp index to destination
-  const tmpPrefix = `${name}[${TMP}]`
-  moveFields(name, tmpPrefix, to, state)
+    // Keep this field that does not match the name,
+    // or has index smaller or larger than affected range
+    newFields[key] = state.fields[key]
+  })
 
-  restoreFunctions(state, backupState)
+  state.fields = newFields
 }
 
 export default move
